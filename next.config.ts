ace7213abcad1c610e00757052cc03cd8d15e21b
danieldaudu65 <danieldaudu65@gmail.com>
2025-06-30 17:51:22 +0100

    block website first commmit

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Tells Next.js to generate static files
-  reactStrictMode: true, // Enables React strict mode
-  swcMinify: true, // Uses SWC for faster minification
+  output: "export", 
   images: {
-    unoptimized: true, // Disable image optimization (Netlify doesn't support Next.js built-in optimization)
+    unoptimized: true, 
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"], // Allows importing SVGs as React components
-    });
-
-    return config;
-  },
+  trailingSlash: true, 
 };
 
 export default nextConfig;
